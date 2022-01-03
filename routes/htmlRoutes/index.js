4ed1dba1614df3816c9e2aa17d6c6bc6327533eb
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-01-03 01:12:40 -0600

    added front end and refactored server calls

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,21 @@
+//adds routes to other files in folder for the server
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+router.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
+});
+
+router.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
